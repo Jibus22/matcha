055e8f583e1cf1a00e362738af4022eee6e71c8f
Le Corre <jibeguitare@hotmail.fr>
2023-11-08 13:03:01 +0100

    ✨ feat(registration): add gender form

--- front/src/routes/register/utils.ts
+++ front/src/routes/register/utils.ts
@@ -1,6 +1,9 @@
 import { redirect } from "react-router-dom";
 import { apiGetUser } from "../controllers/user";
 
+/**
+ * Route guard. Redirect to the right route according to the registration process
+ */
 export const registrationLoader = (reg: RegExp) => {
   return async () => {
     const user = apiGetUser();
--- front/src/routes/root/root.tsx
+++ front/src/routes/root/root.tsx
@@ -14,7 +14,6 @@ export async function loader() {
 }
 
 export async function action() {
-  // Lancer une requete "/api/signout"
   apiSignout();
   return redirect("/auth/signin");
 }
